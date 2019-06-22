955ad669a710d98b10fdc5affce120a3825c4003
verdin <38482622+verdin@users.noreply.github.com>
2019-06-22 03:06:58 -0700

    can create list

--- makelist.py
+++ makelist.py
@@ -3,22 +3,34 @@ from sh import bash
 import sys
 
 listencoded = [];
+code = [];
 encoded = "";
 line = "";
 i = 0;
 j = 0;
 
+print ( "running");
+
 with open('list-cities.txt') as input_file:
-    for i, line in enumerate(input_file):
-        line = line.rstrip()
-        if line:
-            print (line);
-            #encoded = ""+ (sh.phantomjs("run.js", line));
-            j = 0;
-            for line in (sh.phantomjs("run.js", line, _iter="out")): # tail("-f", "/var/log/some_log_file.log", _iter=True):
-                #print (j);
-                listencoded.append(line);
-                j += 1;
-            print ( listencoded[0].rstrip())
-            #listencoded = encoded.read().isplitlines()
-            #print (encoded[0]);
+    with open('codes.cvs', 'w') as output_file:
+        for i, line in enumerate(input_file):
+            line = line.rstrip()
+            if line:
+                #print (line);
+                #encoded = ""+ (sh.phantomjs("run.js", line));
+                j = 0;
+                for lineOfencoded in (sh.phantomjs("run.js", line, _iter="out")):
+                    #print (j);
+                    listencoded.append(lineOfencoded);
+                    j += 1;
+                encoded = listencoded[0].rstrip();
+                listencoded[:] = [];
+                #listencoded = encoded.read().isplitlines()
+                #print (encoded);
+                code = encoded.split('"', 4 )
+                #print (code[3]);
+                output = line + "," + code[3].rstrip() + "\n";
+                #print (output);
+                output_file.write(output)
+#{"message":"1010140125015013450"}
+print ( "done");
